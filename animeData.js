319c3aed42686c1349d49ad3e7f0dd07610c8780
H4QYU <abobakr2009g@gmail.com>
2026-02-22 18:20:56 +0100

    Add files via upload

---
+++ animeData.js
@@ -0,0 +1,31 @@
+// animeData.js – كل الأنميات والحلقات بروابط جاهزة للتعبئة
+const animeData = {
+  Boroto: [
+    { episode: "الحلقة 1", url: "https" },
+    { episode: "الحلقة 2", url: "https" },
+    { episode: "الحلقة 3", url: "https" }
+  ],
+  InuYasha: [
+    { episode: "الحلقة 1", url: "https://player.mux.com/Y1Ipbv00mhjH9008G3iEjwV78iRP9dcElbJpVed2UhEUs?metadata-video-title=1&video-title=1" },
+    { episode: "الحلقة 2", url: "https://player.mux.com/H001naDvig2dCnufiu6On016lbtyGDCqxbG5nFVFgkdSg?metadata-video-title=2&video-title=2" },
+    { episode: "الحلقة 3", url: "https://player.mux.com/QPNWhmAJh6LkAwSm3qxaGUWuPpc00sCtmcomdOtlg02xI?metadata-video-title=3&video-title=3" },
+    { episode: "الحلقة 4", url: "https://player.mux.com/BOcQKGZtxYPBddzYD2N02884YsmmncuiUJb5P5mGrqnk?metadata-video-title=4&video-title=4" },
+    { episode: "الحلقة 5", url: "https://player.mux.com/7RE1yQ4mD00D6wt8YGIwmXYedcuPP2wmWGV7FAox5O3k?metadata-video-title=5&video-title=5" },
+    { episode: "الحلقة 6", url: "https://player.mux.com/ZkgHUrA1mBoE5YLX004pabyPCY1Z1vGcgRUkRhIBa00y8?metadata-video-title=6&video-title=6" },
+    { episode: "الحلقة 7", url: "https://player.mux.com/8mjK1Re6O02cF5K68J00yh2Z4xp78BCNiANustq2i3Mtk?metadata-video-title=7&video-title=7" },
+    { episode: "الحلقة 8", url: "https://player.mux.com/wtXQ01vDj1lHFnEu02gWH400jxWLA98hH021288kxH00Zvgo?metadata-video-title=8&video-title=8" },
+    { episode: "الحلقة 9", url: "https://player.mux.com/JusA9AcsB00NTgijUvCb00CDjpG800erizhlRRqMfcghFc?metadata-video-title=9&video-title=9" },
+    { episode: "الحلقة 10", url: "https://player.mux.com/y1bihqYpov8WbfmFG02SEMugukqoRSdXkgpC6i7L9g0100?metadata-video-title=10&video-title=10" },
+    { episode: "الحلقة 11", url: "https://e.pcloud.link/publink/show?code=XZ04s3Z8ibQUPoFGxz28A93j30PYu4dIHMk" },
+    { episode: "الحلقة 12", url: "https://e.pcloud.link/publink/show?code=XZJ4s3ZaD0Y77ERtKS1ModqfFWgvzI5pPeX" }
+  ],
+  Naruto: [
+    { episode: "الحلقة 1", url: "https" },
+    { episode: "الحلقة 2", url: "https" }
+  ],
+  DragonBall: [
+    { episode: "الحلقة 1", url: "https" },
+    { episode: "الحلقة 2", url: "https" }
+  ]
+  // أضف أي أنمي آخر بنفس الطريقة، مع الحلقة وكلمة https للرابط
+};
