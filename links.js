d674531fb4677d0ebbacd6712493e00c88e5cf14
H4QYU <abobakr2009g@gmail.com>
2026-03-12 19:43:16 +0100

    Fix link formatting for InuYasha-3

--- links.js
+++ links.js
@@ -1,7 +1,7 @@
 const links = [
   "https://player.mux.com/Y1Ipbv00mhjH9008G3iEjwV78iRP9dcElbJpVed2UhEUs?metadata-video-title=InuYasha-1",
   "https://player.mux.com/H001naDvig2dCnufiu6On016lbtyGDCqxbG5nFVFgkdSg?metadata-video-title=InuYasha-2",
-  "https://player.mux.com/QPNWhmAJh6LkAwSm3qxaGUWuPpc00sCtmcomdOtlg02xI?metadata-video-title=+InuYasha-3",
+  "https://player.mux.com/QPNWhmAJh6LkAwSm3qxaGUWuPpc00sCtmcomdOtlg02xI?metadata-video-title=InuYasha-3",
   "https://player.mux.com/BOcQKGZtxYPBddzYD2N02884YsmmncuiUJb5P5mGrqnk?metadata-video-title=InuYasha-4",
   "https://player.mux.com/7RE1yQ4mD00D6wt8YGIwmXYedcuPP2wmWGV7FAox5O3k?metadata-video-title=InuYasha-5",
   "https://player.mux.com/ZkgHUrA1mBoE5YLX004pabyPCY1Z1vGcgRUkRhIBa00y8?metadata-video-title=InuYasha-6",
