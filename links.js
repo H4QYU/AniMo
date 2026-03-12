6bd705e8bf1d7b9971443b4b159615853a983dd9
H4QYU <abobakr2009g@gmail.com>
2026-03-12 19:34:00 +0100

    Add files via upload

---
+++ links.js
@@ -0,0 +1,10 @@
+const links = [
+  "https://player.mux.com/Y1Ipbv00mhjH9008G3iEjwV78iRP9dcElbJpVed2UhEUs?metadata-video-title=InuYasha-1",
+  "https://player.mux.com/H001naDvig2dCnufiu6On016lbtyGDCqxbG5nFVFgkdSg?metadata-video-title=InuYasha-2",
+  "https://player.mux.com/QPNWhmAJh6LkAwSm3qxaGUWuPpc00sCtmcomdOtlg02xI?metadata-video-title=+InuYasha-3",
+  "https://player.mux.com/BOcQKGZtxYPBddzYD2N02884YsmmncuiUJb5P5mGrqnk?metadata-video-title=InuYasha-4",
+  "https://player.mux.com/7RE1yQ4mD00D6wt8YGIwmXYedcuPP2wmWGV7FAox5O3k?metadata-video-title=InuYasha-5",
+  "https://player.mux.com/ZkgHUrA1mBoE5YLX004pabyPCY1Z1vGcgRUkRhIBa00y8?metadata-video-title=InuYasha-6",
+  "https://player.mux.com/8mjK1Re6O02cF5K68J00yh2Z4xp78BCNiANustq2i3Mtk?metadata-video-title=InuYasha-7",
+  "https://player.mux.com/wtXQ01vDj1lHFnEu02gWH400jxWLA98hH021288kxH00Zvgo?metadata-video-title=InuYasha-8"
+];
